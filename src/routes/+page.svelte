b3e418121976412fd4ddfe462ad997a1abdea219
Viiihzinha <evi1810007@gmail.com>
2025-01-28 16:46:52 -0400

    teste1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,38 @@
-<h1> Seja Bem Vindo(a)!!</h1>
-<style>
-div {
-    background-color: ##f5d3f1
-}
+<p> Seja Bem Vindo(a)!!</p>
 
-</style>
+<form>
+    <div class="fundo">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+</div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="mb-3 form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+
+
+  <style>
+    .fundo {
+        background-color: aqua;
+        padding: 0;
+        padding-left: 0;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+
+
+    }
+  </style>
+
+
+
+
